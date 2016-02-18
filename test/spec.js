82ab8668b7e594ce6b241bf186e3afb637f3fc46
lrubioromero <luciano.rubioromero@amadeus.com>
2016-02-18 09:19:56 +0000

    Corrected assert actual/expected

--- test/spec.js
+++ test/spec.js
@@ -4,32 +4,32 @@ var converter = require('../src/converter');
 describe('Telephone number conversion', function(){
 
   it('should remove spaces', function () {
-    assert.equal('+44123456789', converter.convertPhoneNumber('+44 123 456 789'));
+    assert.equal(converter.convertPhoneNumber('+44 123 456 789'), '+44123456789');
   });
 
   it('should convert international code to plus form', function () {
-    assert.equal('+44123456789', converter.convertPhoneNumber('0044123456789'));
+    assert.equal(converter.convertPhoneNumber('0044123456789'), '+44123456789');
   });
 
   it('should detect missing international code', function () {
-    assert.equal('Missing international code', converter.convertPhoneNumber('44123456789'));
+    assert.equal(converter.convertPhoneNumber('44123456789'), 'Missing international code');
   });
 
   it('should ignore spaces and symbols', function () {
-    assert.equal('+44123456789', converter.convertPhoneNumber('+44 123-45x6789'));
+    assert.equal(converter.convertPhoneNumber('+44 123-45x6789'), '+44123456789');
   });
 
   it('should ignore numbers in brackets', function () {
-    assert.equal('+44123456789', converter.convertPhoneNumber('+44(0)123456789'));
+    assert.equal(converter.convertPhoneNumber('+44(0)123456789'), '+44123456789');
   });
 
   it('should not convert if number is not given', function () {
-    assert.equal('', converter.convertPhoneNumber());
+    assert.equal(converter.convertPhoneNumber(), '');
   });
 
   it('should ignore characters', function () {
-    assert.equal('+44123456789', converter.convertPhoneNumber('+44123456789 Name'));
-    assert.equal('+44123456789', converter.convertPhoneNumber('+44123456789Name LastName'));
+    assert.equal(converter.convertPhoneNumber('+44123456789 Name'), '+44123456789');
+    assert.equal(converter.convertPhoneNumber('+44123456789Name LastName'), '+44123456789');
   });
 
 });
