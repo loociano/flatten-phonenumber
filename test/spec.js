2d4d9eb7900f57b87cf66260311bf6a142a7862d
lrubioromero <luciano.rubioromero@amadeus.com>
2016-02-18 08:53:55 +0000

    Add spec

--- test/spec.js
+++ test/spec.js
@@ -6,4 +6,17 @@ describe('Telephone number conversion', function(){
   it('should remove spaces', function () {
     assert.equal('+44123456789', converter.convertPhoneNumber('+44 123 456 789'));
   });
+
+  it('should convert international code to plus form', function () {
+    assert.equal('+44123456789', converter.convertPhoneNumber('0044123456789'));
+  });
+
+  it('should ignore spaces and symbols', function () {
+    assert.equal('+44123456789', converter.convertPhoneNumber('+44 123-45x6789'));
+  });
+
+  it('should ignore numbers in brackets', function () {
+    assert.equal('+44123456789', converter.convertPhoneNumber('+44(0)123456789'));
+  });
+
 });
